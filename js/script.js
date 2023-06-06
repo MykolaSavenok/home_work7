877490adfc88fbdda721f66f5991df503c470f29
SavenokNikolay <savenok.kolya.nikolay@gmail.com>
2023-06-06 21:23:54 +0300

    add

--- js/script.js
+++ js/script.js
@@ -14,30 +14,37 @@ alert(sum)
 
 
 //Дано ціле число. З'ясувати, чи є воно простим (простим називається число, більше ніж 1, яке не має інших дільників, крім 1 і себе).
-let number = 10;
+const number = 5;
+let isPrime = true;
 
 for (let i = 2; i <= number; i++) {
-   let isPrime = true;
-   for (let j = 2; j < i; j++) {
-      if (i % j === 0) {
-         isPrime = false;
-         console.log(`${i}: Складене число`)
-         break;
-      }
-   }
-   if (isPrime) {
-      console.log(`${i}: Просте число`);
-   }
+  if (number % i === 0 && i !== number) {
+    isPrime = false;
+    console.log(`${number}: Складене число`);
+    break;
+  }
+}
+
+if (isPrime) {
+  console.log(`${number}: Просте число`);
 }
 
 
 //Дано ціле число. Визначити, чи можна одержати це число шляхом зведення числа 3 в деякий ступінь. (Наприклад, числа 9, 81 можна отримати, а 13 - не можна)
 const base = 3;
 const exponent = 3;
+const targetNumber = 9;
 let result = 1;
+let isAchievable = false;
 
 for (let i = 0; i < exponent; i++) {
   result *= base;
+
+  if (result === targetNumber) {
+    isAchievable = true;
+    break;
+  }
 }
-console.log(result);
+
+console.log(`Число ${targetNumber} ${isAchievable ? 'можна' : 'не можна'} отримати шляхом зведення числа ${base} в ступінь ${exponent}.`);
 
